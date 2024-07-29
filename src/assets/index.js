b2711382748a4c801a1d2ff5e710e4a0a63adf57
mendsalbert <mendsalbert@gmail.com>
2024-07-29 21:12:34 +0000

    beat-cancer-starter-file

---
+++ src/assets/index.js
@@ -0,0 +1,9 @@
+import sun from "./sun.svg";
+import search from "./search.svg";
+import menu from "./menu.svg";
+import loader from "./loader.svg";
+import records from "./records.svg";
+import screening from "./screening.svg";
+import user from "./user.svg";
+import apps from "./apps.svg";
+export { sun, search, menu, loader, records, screening, user, apps };
