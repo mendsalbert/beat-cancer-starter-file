c8909e8f97f8bfb0d4bec9e48999b48009641fc9
tamim-18 <tamim11903060@gmail.com>
2024-08-17 00:50:05 +0600

    create sidebarlink

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { PrivyProvider } from '@privy-io/react-auth'
 import {Route,Routes,useNavigate} from 'react-router-dom'
+import Sidebar from './components/Sidebar'
 const App = () => {
   return (
     <div className="relative flex min-h-screen flex-row bg-[#13131a] p-4">
      {/* Sidebar */}
-     <div className=" relative hidden mr-10 sm:flex"></div>
+     <div className=" relative hidden mr-10 sm:flex">
+      <Sidebar/>
+     </div>
     <div className="mx-auto max-w-[1280px] flex-1 max-sm:w-full sm:pr-5">
       {/* navbar */}
 
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import { navlinks } from '../constants'
+import { sun } from '../assets'
+import { Link, useNavigate } from 'react-router-dom'
+import { IconHeartHandshake } from "@tabler/icons-react";
+
+const Icon=({style,name,imageUrl,isActive,disabled,handleClick})=>{
+    <div className={`h-[48px] w-[48px] rounded-[10px] ${isActive&& isActive===name&& 'bg-[#2c2f32]'} flex items-center justify-center ${style}`}
+    onClick={handleClick}
+
+    >
+        {!isActive&&<img src={imageUrl} alt={name} className='h-6 w-6'/>}
+        {isActive&&<img src={imageUrl} alt={name} className={`h-6 w-6 ${isActive && 'grayscale-0'}`}/>}
+
+    </div>
+
+}
 
 const Sidebar = () => {
+    const navigate=useNavigate()
+    const [active,setActive]=React.useState('dashboard')
   return (
-    <div>
+    <div className=' sticky flex flex-col items-center justify-between top-5 h-[93vh]'>
+
+        <Link to='/'>
+         <div className="rounded-[10px] bg-[#2c2f32] p-2">
+          <IconHeartHandshake size={40} color="#1ec070" className=" " />
+        </div>
+
+        </Link>
       
     </div>
   )
