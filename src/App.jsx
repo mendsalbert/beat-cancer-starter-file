abd72a84524009a32095e032eae7ab01fb9b3bd1
tamim-18 <tamim11903060@gmail.com>
2024-08-18 01:21:37 +0600

    create navbar and mobile navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { PrivyProvider } from '@privy-io/react-auth'
 import {Route,Routes,useNavigate} from 'react-router-dom'
 import Sidebar from './components/Sidebar'
+import Navbar from './components/Navbar'
 const App = () => {
   return (
     <div className="relative flex min-h-screen flex-row bg-[#13131a] p-4">
@@ -11,6 +12,7 @@ const App = () => {
      </div>
     <div className="mx-auto max-w-[1280px] flex-1 max-sm:w-full sm:pr-5">
       {/* navbar */}
+      <Navbar/>
 
       <Routes>
         <Route path="/" element={<div> Home page</div>}/>
